50d8dbe77d25f1029a39a3ab5dbab0c0c2716740
dmacmakes <dmacmakes@gmail.com>
2021-01-19 11:37:10 +1100

    comments?

--- gig_main.go
+++ gig_main.go
@@ -12,12 +12,13 @@ const (
 )
 
 func main() {
-	// here we go.
+	// SDL, an external simple media library, needs initialization.
 	err := sdl.Init(sdl.INIT_EVERYTHING)
 	if err != nil {
 		fmt.Println("Attempting sdl.INIT_EVERYTHING: ", err)
 		return
 	}
+	// Ask SDL to create and show us a window
 	window, err := sdl.CreateWindow(
 		"GIG follow along!",
 		sdl.WINDOWPOS_UNDEFINED, sdl.WINDOWPOS_UNDEFINED, // location
